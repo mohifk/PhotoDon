c2203c2e9fc92b53a9571fd9f95dbfc5a3a2cacf
mohifk <mohi.fk@hotmail.com>
2022-10-26 17:54:38 +0330

    create first config for website

---
+++ PhotoDon/urls.py
@@ -0,0 +1,45 @@
+"""myfirstsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from PhotoDon.settings import STATIC_URL
+from website.sitemaps import StaticViewSitemap
+from blog.sitemaps import BlogSitemap
+sitemaps={
+    'static':StaticViewSitemap,
+    'blog':BlogSitemap
+}
+
+urlpatterns = [
+    path('admin/', admin.site.urls), 
+    path ('',include('website.urls')),
+    path('blog/',include('blog.urls')),
+    path ('accounts/', include('accounts.urls')),
+    path ('accounts/', include('django.contrib.auth.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', include('robots.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('summernote/', include('django_summernote.urls')),
+    path('captcha/', include('captcha.urls')),
+    ]
+    
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
---
+++ website/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
+from website.forms import ContactForm, NameForm,NewsletterForm
+from website.models import Contact
+from django.contrib import messages
+
+def Home_page_view(request):
+    return render(request,'.\website_temp\index.html')
+
+def about_view(request):
+    return render(request ,'.\website_temp\\about.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            Contact=form.save(commit=False)
+            Contact.name='unknown'
+            Contact.save()
+            
+            messages.add_message(request,messages.SUCCESS,'OK Dude your tiket submit SUCCESS')
+        else:
+            messages.add_message(request,messages.ERROR,'Sorry Dude your tiket dident submit')
+    form = ContactForm()
+    return render(request,'.\website_temp\contact.html',{'form':form})
